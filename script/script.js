41367ca7340265ef068d3d415201b0033713efb5
Samuel Yasin <samuelyasin4@gmail.com>
2025-10-15 15:31:52 -0300

    Atualizei o script.js
    Alterei as variáveis que mando para a função cardDados para um objet. Criei a função ApagarConteudo

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 document.querySelector('#buscar').addEventListener('click', async (Event) => {
     Event.preventDefault()
-    const cidadeNome = document.querySelector('#cidade').value
+    var cidadeNome = document.querySelector('#cidade').value
     if (!cidadeNome) {
         alert('Preencha o nome da cidade')
     }
@@ -9,28 +9,32 @@ document.querySelector('#buscar').addEventListener('click', async (Event) => {
     var apiUrl = `https://dataservice.accuweather.com/locations/v1/cities/search?q=${encodeURI(cidadeNome)}&language=pt-br&details=true`
     var results = await fetch(apiUrl, apiKey)
     var json = await results.json()
-    var cidadeCode = json[0].Key
-    var pais = json[0].Country.ID
+    var cidadeDados = {nome: json[0].EnglishName, code: json[0].Key, pais: json[0].Country.ID}
 
     // Recolhendo os dados climáticos atráves do código da cidade
     var apiKey = {method: 'GET', headers: {Authorization: 'Bearer zpka_a7b5081ba5434130baa1bee2334e9c9c_2752036a'}}
-    var apiUrl = `https://dataservice.accuweather.com/currentconditions/v1/${encodeURI(cidadeCode)}?language=pt-br`
+    var apiUrl = `https://dataservice.accuweather.com/currentconditions/v1/${encodeURI(cidadeDados.code)}?language=pt-br`
     
     var results = await fetch(apiUrl, apiKey)
     var json = await results.json()
 
-    cardDados(json, pais)
+    cardDados(json, cidadeDados)
 })
 
-function cardDados(json, pais) {
+function cardDados(json, cidadeDados) {
     const temperatura = json[0].Temperature.Metric.Value
     const tempo = json[0].WeatherText
     const divCentral = document.querySelector('#centralResultadosBusca')
+    apagarConteudo()
     divCentral.innerHTML += `
     <arcticle>
-        <h2>${document.querySelector('#cidade').value}</h2>
-        <small>${pais}</small>
+        <h2>${cidadeDados.nome}</h2>
+        <small>${cidadeDados.pais}</small>
         <p>Temp. ${temperatura} </br> Tempo: ${tempo}</p>
     </arcticle>
     `
+}
+
+function apagarConteudo() {
+    document.getElementById('cidade').value = ''
 }
