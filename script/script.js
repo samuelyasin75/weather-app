e8813b4a995e4eccd784f1ac6638373081441047
Samuel Yasin <samuelyasin4@gmail.com>
2025-10-16 11:09:48 -0300

    Atualizei o script.js
    mudei o nome do arcticle para articcle e adicionei o parametro de se é dia ou não

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 document.querySelector('#buscar').addEventListener('click', async (Event) => {
     Event.preventDefault()
     var cidadeNome = document.querySelector('#cidade').value
+    apagarConteudo()
     if (!cidadeNome) {
         alert('Preencha o nome da cidade')
     }
@@ -9,7 +10,7 @@ document.querySelector('#buscar').addEventListener('click', async (Event) => {
     var apiUrl = `https://dataservice.accuweather.com/locations/v1/cities/search?q=${encodeURI(cidadeNome)}&language=pt-br&details=true`
     var results = await fetch(apiUrl, apiKey)
     var json = await results.json()
-    var cidadeDados = {nome: json[0].EnglishName, code: json[0].Key, pais: json[0].Country.ID}
+    var cidadeDados = {nome: json[0].EnglishName, code: json[0].Key, pais: json[0].Country.ID, isDay: function () {if(json[0].IsDayTime == true) {return '&#x1F506;'} else { return '&#x1F319;'}}} 
 
     // Recolhendo os dados climáticos atráves do código da cidade
     var apiKey = {method: 'GET', headers: {Authorization: 'Bearer zpka_8111cf3b21b2491cbd9a140cfe47a967_04b7d335'}}
@@ -17,7 +18,7 @@ document.querySelector('#buscar').addEventListener('click', async (Event) => {
     
     var results = await fetch(apiUrl, apiKey)
     var json = await results.json()
-
+    console.log(json)
     cardDados(json, cidadeDados)
 })
 
@@ -27,11 +28,11 @@ function cardDados(json, cidadeDados) {
     const divCentral = document.querySelector('#centralResultadosBusca')
     apagarConteudo()
     divCentral.innerHTML += `
-    <arcticle>
+    <article>
         <h2>${cidadeDados.nome}</h2>
         <small>${cidadeDados.pais}</small>
-        <p>Temp. ${temperatura} </br> Tempo: ${tempo}</p>
-    </arcticle>
+        <p>Horário: ${cidadeDados.isDay()}Temp. ${temperatura} </br> ${tempo}</p>
+    </article>
     `
 }
 
