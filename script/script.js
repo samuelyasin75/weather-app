456c27f565964fc94aa3a6d8f8b05fdfa31e1bab
Samuel Yasin <samuelyasin4@gmail.com>
2025-10-17 11:27:12 -0300

    Atualizei o script.js
    Ajustei o object cidadeDados, criei três functions para recolher dados e igualar um emoji de acordo com o dado recolhido.

--- script/script.js
+++ script/script.js
@@ -10,7 +10,7 @@ document.querySelector('#buscar').addEventListener('click', async (Event) => {
     var apiUrl = `https://dataservice.accuweather.com/locations/v1/cities/search?q=${encodeURI(cidadeNome)}&language=pt-br&details=true`
     var results = await fetch(apiUrl, apiKey)
     var json = await results.json()
-    var cidadeDados = {nome: json[0].EnglishName, code: json[0].Key, pais: json[0].Country.ID, isDay: function () {if(json[0].IsDayTime == true) {return '&#x1F506;'} else { return '&#x1F319;'}}} 
+    var cidadeDados = {nome: json[0].EnglishName, code: json[0].Key, pais: json[0].Country.ID} 
 
     // Recolhendo os dados climáticos atráves do código da cidade
     var apiKey = {method: 'GET', headers: {Authorization: 'Bearer zpka_8111cf3b21b2491cbd9a140cfe47a967_04b7d335'}}
@@ -22,16 +22,43 @@ document.querySelector('#buscar').addEventListener('click', async (Event) => {
     cardDados(json, cidadeDados)
 })
 
+// functions para determinar emojis de acordo com os dados da cidade
+function diaOuNoite(json) {
+    if (json[0].IsDayTime == true) {
+        return '&#x1F506; Dia'
+    } else {
+        return '&#x1F319; Noite'
+    }
+}
+
+function temperatCidade(json) {
+    var tempCidade = json[0].Temperature.Metric.Value
+    if (tempCidade >= 22) {
+        var emote = '&#x1F525;'
+    } else {
+        var emote = '&#x1F9CA;'
+    }
+
+    return `${emote} ${tempCidade}°C`
+}
+
+function tempoCidade(json) {
+    var tempo = json[0].WeatherIcon
+    if (tempo >= 6) {
+        return '&#x1F325; Nublado'
+    } else {
+        return '&#x2600; Ensolarado'
+    }
+}
+
 function cardDados(json, cidadeDados) {
-    const temperatura = json[0].Temperature.Metric.Value
-    const tempo = json[0].WeatherText
     const divCentral = document.querySelector('#centralResultadosBusca')
     apagarConteudo()
     divCentral.innerHTML += `
     <article>
         <h2>${cidadeDados.nome}</h2>
         <small>${cidadeDados.pais}</small>
-        <p>Horário: ${cidadeDados.isDay()}Temp. ${temperatura} </br> ${tempo}</p>
+        <p>${diaOuNoite(json)} <br> ${temperatCidade(json)} </br> ${tempoCidade(json)}</p>
     </article>
     `
 }
